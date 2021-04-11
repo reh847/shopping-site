e04e87e7477b58569deabc6d8ececc2118003deb
reh847 <81152811+reh847@users.noreply.github.com>
2021-04-11 17:19:10 +0530

    add shopping site

---
+++ script.js
@@ -0,0 +1,5 @@
+// document.getElementByTagName("btn).style.color="orange";
+document.querySelector("button").style.color="blue";
+let myName = localStorage.getItem('name');
+localStorage.removeItem('name');
+let myName = localStorage.getItem('name');
